be26232cf0d77eab497c513700da5ccee3398f3e
yusaemi <dokodemoemi@gmail.com>
2024-05-02 16:09:54 +0800

    🐛 Resolve bug
    - fix
        - flow progression print wrong visible
    - Add
        - icon
        - launch4j-plugin

--- src/main/java/idv/module/ui/ProblemFlow.java
+++ src/main/java/idv/module/ui/ProblemFlow.java
@@ -33,13 +33,14 @@ public final class ProblemFlow {
 
         while (isRestart) {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.println(isErrorPara ? "\n※Invalid parameter!※\n" : "");
             System.out.println("【Enter 'exit' to exit this loop at anytime.】");
-            System.out.println(isErrorPara ? "※Invalid parameter!※" : "");
             System.out.println("【automatic： 1 / manual： 2】");
             System.out.print("Please choose mode： ");
             try {
                 isErrorPara = false;
                 String input = InputUtils.getInput(br);
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
                 switch (input) {
                     case "1" -> {
                         while (true) {
@@ -47,19 +48,10 @@ public final class ProblemFlow {
                             data = new ProblemData();
                             System.out.print("Please key in frequency： ");
                             input = InputUtils.getInput(br);
-                            if (input.matches("\\D+")) {
-                                System.out.println("\n※Invalid parameter!※\n");
-                                continue;
-                            }
                             if ("0".equals(input)) {
                                 break;
                             }
-                            int frequency = Integer.parseInt(input);
-                            if (frequency < 200 || frequency > 10000000) {
-                                System.out.println("\n※Invalid parameter!※\n");
-                                continue;
-                            }
-                            autoStart(frequency);
+                            autoStart(input);
                         }
                     }
                     case "2" -> {
@@ -72,24 +64,10 @@ public final class ProblemFlow {
                             Thread.sleep(1);
                             System.out.print("Please select the answer： ");
                             input = InputUtils.getInput(br);
-                            if (!input.matches("[0-3]")) {
-                                System.out.println("\n※Invalid parameter!※\n");
-                                continue;
-                            }
                             if ("0".equals(input)) {
                                 break;
                             }
-                            boolean isPickOn = manuallyStart(input);
-                            manuallySuccess.set(isPickOn ? manuallySuccess.incrementAndGet() : manuallySuccess.get());
-                            manuallyFailure.set(!isPickOn ? manuallyFailure.incrementAndGet() : manuallyFailure.get());
-                            System.out.println(isPickOn ? "\nYou win!\n" : "\nYou lose!\n");
-                            Thread.sleep(1);
-                            String rate = new BigDecimal(Double.toString(manuallySuccess.get())).divide(new BigDecimal(Double.toString(count.get())), 10, RoundingMode.HALF_UP) + "";
-                            NumberFormat nf = NumberFormat.getPercentInstance();
-                            nf.setMinimumFractionDigits(2);
-                            System.out.println("Sequence number of play： " + count.get());
-                            System.out.println("Average hit rate： " + nf.format(Double.parseDouble(rate)) + "\n");
-                            count.incrementAndGet();
+                            manuallyStart(input, manuallySuccess, manuallyFailure, count);
                         }
                     }
                     default -> isErrorPara = true;
@@ -100,29 +78,54 @@ public final class ProblemFlow {
         }
     }
 
-    private static boolean manuallyStart(String position) throws InterruptedException, IOException {
+    private static void manuallyStart(String position, AtomicInteger manuallySuccess, AtomicInteger manuallyFailure, AtomicInteger count) throws InterruptedException, IOException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        if (!position.matches("[0-3]")) {
+            System.out.println("\n※Invalid parameter!※\n");
+            return;
+        }
         List<String> select = new ArrayList<>();
         List<String> result = new ArrayList<>();
         IntStream.range(0, 3).forEach(v -> {
-            result.add("pickOn".equals(data.getPools().get(v)) ? "★" : "☆");
+            result.add("pickOn".equals(data.getPools().get(v)) ? "羊" : "車");
             select.add(Integer.toString(v + 1).equals(position) ? "√" : " ");
         });
         System.out.println("\t" + select.get(0) + "\t\t" + select.get(1) + "\t\t" + select.get(2));
-        // System.out.println("╒══╤══╤══╕");
+        System.out.println("╒═══════════════╤═══════════════╤═══════════════╕");
         System.out.println("|\t" + result.get(0) + "\t|\t" + result.get(1) + "\t|\t" + result.get(2) + "\t|");
-        // System.out.println("╘══╧══╧══╛");
-        return "pickOn".equals(data.getPools().get(Integer.parseInt(position) - 1));
+        System.out.println("╘═══════════════╧═══════════════╧═══════════════╛");
+        boolean isPickOn = "pickOn".equals(data.getPools().get(Integer.parseInt(position) - 1));
+        manuallySuccess.set(isPickOn ? manuallySuccess.incrementAndGet() : manuallySuccess.get());
+        manuallyFailure.set(!isPickOn ? manuallyFailure.incrementAndGet() : manuallyFailure.get());
+        System.out.println(isPickOn ? "\nYou win!\n" : "\nYou lose!\n");
+        Thread.sleep(1);
+        String rate = new BigDecimal(Double.toString(manuallySuccess.get())).divide(new BigDecimal(Double.toString(count.get())), 10, RoundingMode.HALF_UP) + "";
+        NumberFormat nf = NumberFormat.getPercentInstance();
+        nf.setMinimumFractionDigits(2);
+        System.out.println("Sequence number of play： " + count.get());
+        System.out.println("Average hit rate： " + nf.format(Double.parseDouble(rate)) + "\n");
+        count.incrementAndGet();
     }
 
-    private static void autoStart(int frequency) throws IOException, InterruptedException {
+    private static void autoStart(String input) throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        if (input.matches("\\D+")) {
+            System.out.println("\n※Invalid parameter!※\n");
+            return;
+        }
+        BigDecimal frequency = new BigDecimal(input);
+        if (frequency.compareTo(new BigDecimal(200)) < 0 || frequency.compareTo(new BigDecimal(10000000)) > 0) {
+            System.out.println("\n※Invalid parameter!※\n");
+            return;
+        }
         AtomicInteger autoSuccess = new AtomicInteger();
         AtomicInteger autoFailure = new AtomicInteger();
-        IntStream.range(0, frequency).forEach(v -> {
+        IntStream.range(0, frequency.intValueExact()).forEach(v -> {
             boolean isPickOn = "pickOn".equals(data.getPools().get((int) (Math.random() * data.getPools().size())));
             autoSuccess.set(isPickOn ? autoSuccess.incrementAndGet() : autoSuccess.get());
             autoFailure.set(!isPickOn ? autoFailure.incrementAndGet() : autoFailure.get());
         });
-        String rate = new BigDecimal(Double.toString(autoSuccess.get())).divide(new BigDecimal(Double.toString(frequency)), 10, RoundingMode.HALF_UP).toPlainString();
+        String rate = new BigDecimal(Double.toString(autoSuccess.get())).divide(frequency, 10, RoundingMode.HALF_UP).toPlainString();
         NumberFormat nf = NumberFormat.getPercentInstance();
         nf.setMinimumFractionDigits(2);
         System.out.println("Mode: automatic\nFrequency: " + frequency);
@@ -130,7 +133,6 @@ public final class ProblemFlow {
     }
 
     private static void playManuallyMode() throws InterruptedException, IOException {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         System.out.println("----------------------------");
         System.out.println("Play game on manually mode");
         System.out.println("----------------------------");
@@ -139,7 +141,6 @@ public final class ProblemFlow {
     }
 
     private static void playAutomaticMode() throws InterruptedException, IOException {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         System.out.println("----------------------------");
         System.out.println("Play game on automatic mode");
         System.out.println("----------------------------");
